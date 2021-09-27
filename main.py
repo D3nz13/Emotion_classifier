c3112335de10db705c959ff128648ff9946ec8c9
D3nz13 <seba.szczepan2@gmail.com>
2021-09-27 14:39:24 +0200

    Created functions responsible for face detection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 import matplotlib.pyplot as plt
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 def display_camera():
     """
     This function captures the image from the camera and displays it. ESC button stops the loop
@@ -9,7 +11,9 @@ def display_camera():
 
     while True:
         _, image = cam.read()
-        cv2.imshow('my cam', image)
+        faces = detect_faces(image)
+        image_cont = contour_faces(image, faces)
+        cv2.imshow('my cam', image_cont)
         if cv2.waitKey(1) == 27:  # ESC
             break
 
@@ -22,10 +26,35 @@ def get_one_frame():
     """
     cam = cv2.VideoCapture(0)
     _, image = cam.read()
-    plt.imshow(image)
+    faces = detect_faces(image)
+    image_cont = contour_faces(image, faces)
+    plt.imshow(image_cont)
     plt.show()
 
     cv2.destroyAllWindows()
+
+
+def detect_faces(image):
+    """
+    This function detects faces using a cascade classifier
+    """
+    image_copy = image.copy()
+    gray = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    return faces
+
+
+def contour_faces(image, faces):
+    """
+    This function draws rectangles around the faces
+    """
+    image_copy = image.copy()
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image_copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    return image_copy
 
 if __name__ == '__main__':
     display_camera()
